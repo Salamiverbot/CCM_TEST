28b063a566f4380a23d94776ffdf77f1895200b1
RobinDHBW <thyeria@gmx.de>
2016-12-22 10:36:03 +0100

    teständerung

--- src/main/java/com/test/CCM/backend/CCM_Constants.java
+++ src/main/java/com/test/CCM/backend/CCM_Constants.java
@@ -3,7 +3,7 @@ package com.test.CCM.backend;
 public interface CCM_Constants {
 	
 	public static final String APPLICATION_NAME = "CCM";
-	public static final String VIEW_NAME_START = "Start";
+	public static final String VIEW_NAME_START = "Startpage";
 	public static final String VIEW_NAME_USER = "User";
 	public static final String VIEW_NAME_MEETING = "Meeting";
 	public static final String VIEW_NAME_ENTERPRISE = "Enterprise";
